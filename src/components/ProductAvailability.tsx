49a0f794db2e1b6e2f055e8c20f170182a7c3add
Asta1984 <salilmandal908@gmail.com>
2025-03-28 21:13:13 +0530

    feedback-ux

--- src/components/ProductAvailability.tsx
+++ src/components/ProductAvailability.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect,  } from 'react';
+import { X } from 'lucide-react';
 import { Calendar } from '../components/ui/calendar';
 import { DateRange } from 'react-day-picker';
 import useAvailabilityStore from '../store/useAvailabilityStore';
@@ -94,7 +95,7 @@ export function ProductAvailability() {
               className="ml-2 text-gray-500 hover:text-gray-700"
               disabled={isLoading}
             >
-             x 
+             <X className="w-4 h-4 text-gray-400" />
             </button>
           </div>
         ))}
--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -26,13 +26,13 @@ function Calendar({
     ),
     weekday: "h-9 w-9 p-0 text-xs font-medium text-muted-foreground/80",
     day_button:
-      "relative flex h-9 w-9 items-center justify-center whitespace-nowrap rounded-lg p-0 text-foreground outline-offset-2 group-[[data-selected]:not(.range-middle)]:[transition-property:color,background-color,border-radius,box-shadow] group-[[data-selected]:not(.range-middle)]:duration-150 focus:outline-none group-data-[disabled]:pointer-events-none focus-visible:z-10 hover:bg-accent group-data-[selected]:bg-primary hover:text-foreground group-data-[selected]:text-primary-foreground group-data-[disabled]:text-foreground/30 group-data-[disabled]:line-through group-data-[outside]:text-foreground/30 group-data-[outside]:group-data-[selected]:text-primary-foreground focus-visible:outline focus-visible:outline-2 focus-visible:outline-ring/70 group-[.range-start:not(.range-end)]:rounded-e-none group-[.range-end:not(.range-start)]:rounded-s-none group-[.range-middle]:rounded-none group-data-[selected]:group-[.range-middle]:bg-accent group-data-[selected]:group-[.range-middle]:text-foreground",
+      "relative flex h-9 w-9 items-center justify-center whitespace-nowrap rounded-full p-0 text-foreground outline-offset-2 group-[[data-selected]:not(.range-middle)]:[transition-property:color,background-color,border-radius,box-shadow] group-[[data-selected]:not(.range-middle)]:duration-150 focus:outline-none group-data-[disabled]:pointer-events-none focus-visible:z-10 hover:bg-red-500 group-data-[selected]:bg-red-500 hover:text-foreground group-data-[selected]:text-primary-foreground group-data-[disabled]:text-foreground/30 group-data-[disabled]:line-through group-data-[outside]:text-foreground/30 group-data-[outside]:group-data-[selected]:text-primary-foreground focus-visible:outline focus-visible:outline-2 focus-visible:outline-ring/70 group-[.range-start:not(.range-end)]:rounded-full group-[.range-end:not(.range-start)]:rounded-full group-[.range-middle]:rounded-full group-data-[selected]:group-[.range-middle]:bg-red-100 group-data-[selected]:group-[.range-middle]:text-foreground",
     day: "group h-9 w-9 px-0 text-sm",
     range_start: "range-start",
     range_end: "range-end",
     range_middle: "range-middle",
     today:
-      "*:after:pointer-events-none *:after:absolute *:after:bottom-1 *:after:start-1/2 *:after:z-10 *:after:h-[3px] *:after:w-[3px] *:after:-translate-x-1/2 *:after:rounded-full *:after:bg-primary [&[data-selected]:not(.range-middle)>*]:after:bg-background [&[data-disabled]>*]:after:bg-foreground/30 *:after:transition-colors",
+      "*:after:pointer-events-none *:after:absolute *:after:bottom-1 *:after:start-1/2 *:after:z-10 *:after:h-[3px] *:after:w-[3px] *:after:-translate-x-1/2 *:after:rounded-full *:after:bg-primary [&[data-selected]:not(.range-middle)>*]:after:bg-background [&[data-disabled]>*]:after:bg-foreground/30 *:after:transition-colors h-9 w-9 rounded-lg bg-accent",
     outside: "text-muted-foreground data-selected:bg-accent/50 data-selected:text-muted-foreground",
     hidden: "invisible",
     week_number: "h-9 w-9 p-0 text-xs font-medium text-muted-foreground/80",
